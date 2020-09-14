1ebcdc3f207b36c570c94c5c21cd3c2b35809f5c
Aaron Parkening <aaron.parkening@gmail.com>
2020-09-14 11:50:24 -0400

    Display controller content in twig template

--- modules/custom/migration_dashboard/src/Controller/DashboardController.php
+++ modules/custom/migration_dashboard/src/Controller/DashboardController.php
@@ -2,16 +2,29 @@
 
 namespace Drupal\migration_dashboard\Controller;
 
-// use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\Response;
+use Drupal\Core\Controller\ControllerBase;
+// use Symfony\Component\HttpFoundation\Response;
 
-// class DashboardController extends ControllerBase {
-class DashboardController {
+class DashboardController extends ControllerBase {
+// class DashboardController {
 
-  public function dash() {
-    $shout = 'Testing 1 2 3';
-        
-    return new Response($shout);
+  public function content() {
+
+    // Set reactDiv
+    $reactDiv = '<div id="react-app">React app will load here.</div>';
+
+    return [
+      '#theme' => 'migration_dashboard_theme',      
+      '#markup' => $reactDiv,
+    ];
+    
   }
+
+
+  // public function dash() {
+  //   $shout = 'Testing nope';
+        
+  //   return new Response($shout);
+  // }
 
 }
